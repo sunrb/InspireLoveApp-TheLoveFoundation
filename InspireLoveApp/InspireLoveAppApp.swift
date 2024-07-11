2ed4bb61026da0cea9c7892f7e028d9750c37746
Rubing <rubing@Rubings-MacBook-Pro.local>
2024-07-11 10:19:10 -0700

    Empty xcode project

---
+++ InspireLoveApp/InspireLoveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspireLoveAppApp.swift
+//  InspireLoveApp
+//
+//  Created by Rubing on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct InspireLoveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
